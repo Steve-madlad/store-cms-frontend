f7f830d0decd861456d8d7e02f848b378ae1af0c
Estifanos Wendimu <teffsauce2@gmail.com>
2025-08-06 23:13:52 +0300

    added dependancy & global styles, completed details page

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -25,6 +25,10 @@ function ProductAndGridSkeleton() {
 
 import { getSingleProduct } from "@/actions/productActions";
 import ProductGrid from "@/components/productsGrid";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { currencyFormat } from "@/lib/utils";
+import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import { Suspense } from "react";
 
@@ -43,15 +47,66 @@ async function ProductContent({ productId }: { productId: string }) {
   return (
     <div className="space-y-10">
       {/* Product Details */}
-      <div className="container mx-auto h-[50vh]">
-        <h1>{product?.name}</h1>
-        <p>{product?.price}</p>
-        <div className="relative aspect-video h-50 rounded-lg shadow-md">
-          <Image
-            fill
-            src={product?.images[0].url || "/placeholder.svg"}
-            alt="product"
-          />
+      <div className="col container mx-auto border-b pb-5 sm:!flex-row">
+        <Tabs defaultValue={product?.images[0].id} className="flex-1">
+          {product?.images.map((image) => (
+            <TabsContent
+              key={image.id}
+              className="relative mb-4 aspect-square w-full rounded-md border bg-gray-100 shadow-xs"
+              value={image.id}
+            >
+              <Image
+                src={image.url}
+                alt="product"
+                fill
+                className="object-contain"
+              />
+            </TabsContent>
+          ))}
+          <TabsList className="flex h-fit gap-2 bg-transparent">
+            {product?.images.map((image) => (
+              <TabsTrigger
+                key={image.id}
+                value={image.id}
+                className="bg-muted cursor group overflow-hiddencursor focus-visible:ring-muted data-[state=active]:ring-primary relative aspect-square h-12.5 !rounded-sm border-0 ring-transparent !outline-0 transition data-[state=active]:ring-1"
+              >
+                <Image
+                  src={image.url}
+                  alt="product"
+                  fill
+                  quality={20}
+                  className="scale-105 object-contain transition-transform group-hover:scale-110"
+                />
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+
+        <div className="flex-1 sm:px-6">
+          <div className="space-y-1 border-b pb-2">
+            <h1 className="text-2xl font-semibold">{product?.name}</h1>
+            <p className="text-lg font-semibold">
+              {currencyFormat.format(Number(product?.price))}
+            </p>
+          </div>
+
+          <div className="my-3 mb-6 space-y-2">
+            <p className="flex gap-4">
+              <b>Size:</b> {product.size.name}
+            </p>
+            <div className="align-center gap-3">
+              <b>Color:</b>
+              <span
+                title={product.color.value}
+                style={{ backgroundColor: product.color.value }}
+                className="border-primary block size-7 rounded-full border"
+              ></span>
+            </div>
+          </div>
+
+          <Button className="cursor rounded-full !px-5">
+            Add to Cart <ShoppingCart />
+          </Button>
         </div>
       </div>
 
